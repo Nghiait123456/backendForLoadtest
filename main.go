f26d321f96c1492e091b637d395dc09168fc0e9c
nghiapm <nghiapm@appota.com>
2022-07-02 16:58:22 +0700

    add get public Ip

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type ipLogs struct {
+	IpSoucrce  string
+	IpForwards []string
+}
 
 func main() {
 	app := fiber.New()
@@ -11,6 +18,15 @@ func main() {
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("homepage")
+	})
+
+	app.Get("/ipSource", func(c *fiber.Ctx) error {
+		ip := ipLogs{
+			IpSoucrce:  c.IP(),
+			IpForwards: c.IPs(),
+		}
+
+		return c.JSON(ip)
 	})
 
 	app.Listen(":8000")
